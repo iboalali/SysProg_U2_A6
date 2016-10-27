121f74ed755196cb12e372dd7f73d74f656d825a
Bullet6020 <elmuches@googlemail.com>
2016-10-27 23:32:39 +0200

    -Added StringFuncs.c & StringFuncs.h -Added basic functions to Stringfuncs.h

---
+++ StringFuncs.h
@@ -0,0 +1,11 @@
+#ifndef _STRINGFUNCS
+#define _STRINGFUNCS
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int namecmp(char *first_string, char *second_string);
+int mystrlen(char *string);
+char* toLower(char *string);
+
+#endif
