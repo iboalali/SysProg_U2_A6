ff8ff321f677311738c6e0f760b80792ba32d914
iboalali <iboalali@gmail.com>
2016-10-29 15:23:33 +0200

    rework PersonenDaten
    Added nodeSortiertHinzufuegen(node *n)
    Added personHinzufuegen(char *vorname, char *nachname)
    removed nodeAmAnfangHinzufuegen(node *n)
    removed personAmEndeHinzufuegen(char *vorname, char *nachname)
    removed personAmAnfangHinzufuegen(char *vorname, char *nachname)

--- PersonenDaten.c
+++ PersonenDaten.c
@@ -1,64 +1,123 @@
 #include "PersonenDaten.h"
+#include "StringFuncs.h"
 
-void personAmAnfangHinzufuegen(char *vorname, char *nachname){
+void personHinzufuegen(char *vorname, char *nachname){
     node *n;
     n = malloc(sizeof(node));
     n->vorname = vorname;
     n->nachname = nachname;
     n->succ = head;
 
-    nodeAmAnfangHinzufuegen(n);
+    nodeSortiertHinzufuegen(n);
 }
 
-void nodeAmAnfangHinzufuegen(node *n){
-	if(head == NULL){
-        head = n;        
-    }
-	else
-    {
-        n->succ=head;
-		head=n;
-    }
-}
-
-void personAmEndeHinzufuegen(char *vorname, char *nachname){
-	node *n;
-    n = malloc(sizeof(node));
-    n->vorname = vorname;
-    n->nachname = nachname;
-    n->succ = NULL;
-
-    nodeAmEndeHinzufuegen(n);	
-}
-
-void nodeAmEndeHinzufuegen(node *n){
-    if(head == NULL){
-        head = n;
+void nodeSortiertHinzufuegen(node *n){
+    if (head == NULL){
+        nodeAmAnfangHinzufuegen(n);
         return;
     }
 
-    struct node *q;
-    q = head;
+    node *p; // head location
+    node *q; // head location + 1
+    p = head; 
+    q = head->succ;
 
-    while(q->succ != NULL){
-        q = q->succ;
+    if(node_count == 1){
+        if (namecmp(n->nachname, head->nachname) == 1){
+            // Neuer Name kommt vor den derzeitigen Name in der liste
+            nodeAmAnfangHinzufuegen(n);
+        }
+        else if(namecmp(n->nachname, head->nachname) == 0){
+            // Neuer Name is exakt gleich
+
+            // Vorname vergleichen
+
+            //nodeAmEndeHinzufuegen(n);
+        }
+        return;
     }
 
+    // if there are more nodes than one
+    while (q->succ != NULL){
+        if (namecmp(n->nachname, p->nachname) == 1){
+            // Neuer Name kommt vor den derzeitigen Name in der liste
+            p->succ = n;
+            n->succ = q;
+            return;
+        }else if(namecmp(n->nachname, p->nachname) == 0){
+            // Der Name is exakt gleich
+
+            // Vorname vergleichen
+        }
+        p = q;
+        q = q->succ;
+
+    }
+
+    // gone through the whole list, just add to the end
+    nodeAmEndeHinzufuegen(n);
+}
+
+void nodeAmAnfangHinzufuegen(node *n){
+    if (head == NULL){
+        head = n;
+        node_count = 1;
+    }
+    else{
+        n->succ = head;
+        head = n;
+        node_count++;
+    }
+}
+
+void nodeAmEndeHinzufuegen(node *n){
+    if (head == NULL){
+        head = n;
+        node_count = 1;
+        return;
+    }
+
+    node *q;
+    q = head;
+
+    while (q->succ != NULL){
+        q = q->succ;
+    }
+    node_count++;
     q->succ = n;
 }
 
+int getNodeCount(){
+    if(head == NULL){
+        return 0;
+    }
+
+    node *q;
+    q = head;
+
+    int i;
+    i = 0;
+
+    while(q == NULL){
+        i++;
+        q = q->succ;
+    }
+
+    return i;
+}
+
 void printListe(node *liste){
-    struct node *q;
+    node *q;
     q = liste;
 
-    if(q == NULL){
+    if (q == NULL){
         printf("%s\n", "Liste ist Leer");
         return;
     }
 
-    while(q != NULL){
+    while (q != NULL){
         printf("%s %s\n", q->vorname, q->nachname);
         q = q->succ;
     }
-	printf("\n");
+    printf("\n");
 }
